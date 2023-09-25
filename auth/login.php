49ec6429c0e3bae69cec52a23d4832ae5deca617
Yufri Luttu <yufridonluttu@gmail.com>
2023-09-25 13:15:15 +0800

    bug fix login page

--- auth/login.php
+++ auth/login.php
@@ -47,7 +47,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <link href="../assets/DataTables/datatables.min.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -117,9 +116,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-</script>
-
-<script src="../assets/DataTables/jquery.js"></script>
-<script src="../assets/DataTables/datatables.min.js"></script>
-<!-- Sweet Alert -->
-<script>
